0cdd88e3f9525126d6aa7d8047ff09f3f3226622
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 17:44:14 -0500

    Create Kudos component.

---
+++ client/src/components/kudos/Kudos.jsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react';
+import { FaEthereum } from 'react-icons/fa';
+import { KudosContext } from '../../context/KudosContext';
+import { shortenAddress } from '../../utils/shortenAddress';
+import "./kudos.css";
+
+const Kudos = ({ senderAddress, receiverAddress, timestamp, message, tokenId }) => {
+  const { state, handler } = useContext(KudosContext);
+
+  const getImageUrl = (tokenId) => {
+    const ipfsId = state.nftsMetadata.find(nft => nft.id === tokenId).image.split('ipfs://')[1];
+    console.log(ipfsId);
+    return `https://ipfs.io/ipfs/${ipfsId}`
+  }
+
+  return (
+    <div className='kudos__kudos-container__kudos'>
+      <div className='kudos__kudos-container__kudos-nft'>
+        <img
+          src={getImageUrl(tokenId)}
+          alt="nft"
+        />
+      </div>
+      <div className='kudos__kudos-container__kudos-data'>
+        <div />
+        <h4>From: </h4>
+        <a href={`https://rinkeby.etherscan.io/address/${senderAddress}`} target="_blank" rel="noreferrer">
+          <p>{shortenAddress(senderAddress.toUpperCase())}</p>
+        </a>
+        <h4>To: </h4>
+        <a href={`https://rinkeby.etherscan.io/address/${receiverAddress}`} target="_blank" rel="noreferrer">
+          <p>{shortenAddress(receiverAddress.toUpperCase())}</p>
+        </a>
+        <h4>Date:</h4>
+        <p>{timestamp}</p>
+        <br />
+        <div />
+        <h4>Message: </h4>
+        <p>{message}</p>
+      </div>
+      <FaEthereum className='kudos__kudos-container__etherscan' />
+
+
+    </div>
+  );
+}
+
+export default Kudos;
