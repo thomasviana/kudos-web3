8415e5f37c4441986d5f02cad67fda0052d30d1f
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 12:00:04 -0500

    Verify contract using truffle.

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -2,39 +2,46 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const mnemonic = require("./secrets.json").mnemonic;
 const infura_api_key = require("./secrets.json").infura_api_key;
+const etherscan_api_key = require("./secrets.json").etherscan_api_key;
 
 module.exports = {
- networks: {
-  development: {
-   host: "127.0.0.1", // Localhost (default: none)
-   port: 7545, // Ganache
-   network_id: "5777", // Ganache
-  },
-  rinkeby: {
-   provider: () =>
-    new HDWalletProvider(
-     mnemonic,
-     `https://rinkeby.infura.io/v3/${infura_api_key}`
-    ),
-   port: 8545,
-   network_id: 4, // Rinkeby's id
-   gas: 5500000, // Rinkeby has a lower block limit than mainnet
-   confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-   timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-  },
- },
- compilers: {
-  solc: {
-   version: "0.8.14", // Fetch exact version from solc-bin (default: truffle's version)
-   // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-   settings: {
-    // See the solidity docs for advice about optimization and evmVersion
-    optimizer: {
-     enabled: true,
-     runs: 200,
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Ganache
+      network_id: "5777", // Ganache
     },
-   },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${infura_api_key}`
+        ),
+      port: 8545,
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
- },
+  compilers: {
+    solc: {
+      version: "0.8.14", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: etherscan_api_key
+  }
 };
