3faf66c5f7d0c3d7b27ae5768d960394ac2b299e
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 17:44:32 -0500

    Update barrel file with kudos and loader.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as CTA } from "./cta/CTA";
+export { default as Kudos } from "./kudos/Kudos";
+export { default as Loader } from "./loader/Loader";
 export { default as Navbar } from "./navbar/Navbar";
 
