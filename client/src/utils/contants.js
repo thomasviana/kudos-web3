e525791f1fce9ef6f170beec3583320fb38c515c
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 11:59:29 -0500

    Add some utils.

---
+++ client/src/utils/contants.js
@@ -0,0 +1,5 @@
+import abi from "./Kudos.json";
+
+export const contractAddress = "0x658ba5066Edc4dCf1A171d9fc4F0Dccb7e430DBe";
+export const contractABI = abi.abi;
+export const networks = abi.networks;
