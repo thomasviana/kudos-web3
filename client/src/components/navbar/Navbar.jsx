fc03cce318ef13fe9d6a04157b28e1c08d332e68
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 12:05:33 -0500

    Change connect button state to connected wallet on navbar.

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,36 @@
+import { useContext } from "react";
+import { GoPrimitiveDot } from "react-icons/go";
 import logo from "../../assets/logo-zemoga.png";
+import { KudosContext } from "../../context/KudosContext";
+import { shortenAddress } from "../../utils/shortenAddress";
 import "./navbar.css";
 
 const Navbar = () => {
- return (
-  <div className='kudos__navbar'>
-   <div className='kudos__navbar-logo'>
-    <img src={logo} alt='logo' />
-   </div>
-   <div className='kudos__navbar-connect_wallet'>
-    <button type='button'>Connect Wallet</button>
-   </div>
-  </div>
- );
+  const { state, handler } = useContext(KudosContext);
+
+  return (
+    <div className='kudos__navbar'>
+      <div className='kudos__navbar-logo'>
+        <img src={logo} alt='logo' />
+      </div>
+      <div className='kudos__navbar-connect_wallet'>
+        {!state.currentAccount ? (
+          <button type='button' onClick={handler.connectWallet}>
+            Connect Wallet
+          </button>
+        ) : (
+          <div
+            className='kudos__navbar-connected_wallet'
+            type='button'
+            onClick={handler.connectWallet}
+          >
+            <GoPrimitiveDot className='kudos__navbar-connected_wallet-circle' />
+            {shortenAddress(state.currentAccount.toUpperCase())}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
