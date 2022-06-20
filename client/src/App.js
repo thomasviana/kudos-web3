351839d9a1cf0e1ee9ff0a08a77f4f077208fc85
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 13:46:49 -0500

    Add opensea cta.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
-import { Navbar } from "./components";
+import { CTA, Navbar } from "./components";
 import { Footer, Header } from "./containers";
 import "./index.css";
 
 const App = () => {
- return (
-  <div className='App'>
-   <div className='gradient__bg'>
-    <Navbar />
-    <Header />
-   </div>
-   <Footer />
-  </div>
- );
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+        <CTA />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/cta/CTA.jsx
@@ -0,0 +1,15 @@
+import "./cta.css";
+
+const CTA = () => (
+  <div className='kudos__cta section__margin'>
+    <div className='kudos__cta-content' >
+      <p>Do you want to know more about MTK?</p>
+      <h3>Take a look at the MoreThanKudos collection at OpenSea</h3>
+    </div>
+    <div className='kudos__cta-btn'>
+      <button type='button'>View at OpenSea</button>
+    </div>
+  </div>
+);
+
+export default CTA;
