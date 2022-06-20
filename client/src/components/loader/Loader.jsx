ff31b813d104dcba044844250724c5b05739cc05
Thomas Viana <thomasviana16@gmail.com>
2022-06-20 17:44:01 -0500

    Create Loader component.

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import "./loader.css";
+
+const Loader = () => (
+  <div className="kudos__spinner-container">
+    <div className="kudos__loading-spinner">
+    </div>
+  </div>
+);
+
+export default Loader;
